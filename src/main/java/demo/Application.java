b6a048d1e5c7e18da63a28db2d5a26bcf88f5471
Lauren Kemperman <lkemperman@codeforamerica.org>
2023-09-07 13:51:32 -0600

    add application and configuration classes

--- src/main/java/demo/Application.java
+++ src/main/java/demo/Application.java
@@ -1,2 +1,35 @@
-package demo;public class Application {
+package demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@SpringBootApplication
+@EnableConfigurationProperties(DemoConfiguration.class)
+public class Application implements CommandLineRunner {
+
+    private final DemoConfiguration configuration;
+
+    public Application(DemoConfiguration configuration) {
+        this.configuration = configuration;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void run(String... args) {
+
+        Logger logger = LoggerFactory.getLogger(Application.class);
+
+        logger.info("----------------------------------------");
+        logger.info("Configuration properties");
+        logger.info("   example.username is {}", configuration.getUsername());
+        logger.info("   example.password is {}", configuration.getPassword());
+        logger.info("----------------------------------------");
+    }
 }
--- src/main/java/demo/DemoConfiguration.java
+++ src/main/java/demo/DemoConfiguration.java
@@ -1,2 +1,31 @@
-package demo;public class DemoConfiguration {
+package demo;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * @author Mark Paluch
+ */
+@ConfigurationProperties("example")
+public class DemoConfiguration {
+
+    private String username;
+
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
+
